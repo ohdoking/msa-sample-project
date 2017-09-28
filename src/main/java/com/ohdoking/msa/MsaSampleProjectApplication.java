14d0b49a402b34c19b19587f229dbc737e72745b
ohdoking <ohdoking@gmail.com>
2017-09-28 21:00:39 +0900

    project init

---
+++ src/main/java/com/ohdoking/msa/MsaSampleProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ohdoking.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaSampleProjectApplication.class, args);
+	}
+}
